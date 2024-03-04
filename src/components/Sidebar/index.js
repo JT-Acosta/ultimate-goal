5cb3bcb55f067ca0a3d1a7b1b2ff19ea4cd9fc7b
Justin Acosta <150075360+JT-Acosta@users.noreply.github.com>
2024-03-03 22:56:15 -0800

    Update index.js
    Updated Sidebar menu and Cart Count

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,7 +1,11 @@
 import React from 'react'
 import { SidebarContainer, Icon, CloseIcon, SidebarWrapper, SidebarMenu, SidebarLink, SideBtnWrap, SidebarRoute } from './SidebarElements'
+import { useCart } from '../Context/CartContext';
 
 const Sidebar = ({ isOpen, toggle }) => {
+
+  const { cartCount } = useCart();
+  
   return (
     <SidebarContainer isOpen={ isOpen } onClick={ toggle }>
       <Icon onClick= { toggle }>
@@ -11,10 +15,11 @@ const Sidebar = ({ isOpen, toggle }) => {
         <SidebarMenu>
             <SidebarLink to='about' onClick={ toggle }>About</SidebarLink>
             <SidebarLink to='contact' onClick={ toggle }>Contact</SidebarLink>
+            <SidebarLink to='journeys' onClick={ toggle }>Journeys</SidebarLink>
             <SidebarLink to='packages' onClick={ toggle }>Packages</SidebarLink>
         </SidebarMenu>
         <SideBtnWrap>
-            <SidebarRoute to='/cart'>Cart</SidebarRoute>
+            <SidebarRoute to='/cart'>Cart ({ cartCount })</SidebarRoute>
         </SideBtnWrap>
       </SidebarWrapper>
     </SidebarContainer>
